7e166aaab17f17493868a690ae375249e0adfedf
Toni <tposcic@riteh.hr>
2017-03-22 21:47:44 +0100

    it werks

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,9 +1,22 @@
 // if you checked "fancy-settings" in extensionizr.com, uncomment this lines
 
+var openTabs = new Array();
+chrome.tabs.query({}, function (tabs) {
+    for (var i = 0; i < tabs.length; i++) {
+        openTabs[i] = tabs[i];
+    }
+    // Moved code inside the callback handler
+    for (var i = 0; i < openTabs.length; i++) {
+        if (openTabs[i] != null)
+           window.console.log(openTabs[i].url);
+        else {
+            window.console.log("??" + i);
+        }
+    }
+});
+/*
 var settings = new Store("settings", {
     "sample_setting": "test"
 });
+*/
 
-chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-    alert(tabs[0].url);
-});

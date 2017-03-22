511d472abc193aae3d48119e2a0260286ca69af0
Toni <tposcic@riteh.hr>
2017-03-22 21:39:31 +0100

    hbfdgjhf

--- src/bg/background.js
+++ src/bg/background.js
@@ -4,20 +4,6 @@ var settings = new Store("settings", {
     "sample_setting": "test"
 });
 
-chrome.extension.onMessage.addListener(function(a,b,s) {
-chrome.tabs.query({
-     'active': true,
-     'lastFocusedWindow': true
- },
- function(tabs) {
-   if (tabs[0]) {
-     alert('chrome.tabs.query: ' + tabs[0].windowId);
-   } else {
-     alert('chrome.tabs.query: NO TAB FOUND!');
-   }
-   if (tabs.length > 1) {
-     alert('chrome.tabs.query: MORE THAN ONE TABs ' + JSON.stringify(tabs));
-   }
- });
- s({});
+chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+    alert(tabs[0].url);
 });
--- src/bg/tabplayer.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-    alert('test');
-});
-
-
-var domListener = function() {
-	chrome.extension.sendMessage({ foo: 'bar'}, function() {});
-};
-window.addEventListener('DOMContentLoaded', domListener, false);

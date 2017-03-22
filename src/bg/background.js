4fe21a865caf03c0d6fc6267b1c8b25d68a56dd7
Toni <tposcic@riteh.hr>
2017-03-22 22:45:06 +0100

    baterijo

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,7 +1,10 @@
 // if you checked "fancy-settings" in extensionizr.com, uncomment this lines
 
+tabAction();
+
 function tabAction(){
     var openTabs = new Array();
+    var youtubeTabs = new Array();
 
     chrome.tabs.query({}, function (tabs) {
         for (var i = 0; i < tabs.length; i++) {
@@ -9,12 +12,15 @@ function tabAction(){
         }
         // Moved code inside the callback handler
         for (var i = 0; i < openTabs.length; i++) {
-            if (openTabs[i] != null && openTabs[i].url.includes("youtube")){
-            window.console.log(openTabs[i].url);
+            if (openTabs[i].url.includes("youtube")){
+                window.console.log(openTabs[i].url);
+                youtubeTabs.push(openTabs[i].url);
             }
         }
-    });
 
+        window.console.log(youtubeTabs);
+
+    });
 }
 
 chrome.tabs.onCreated.addListener(function(tab) {         
@@ -24,10 +30,3 @@ chrome.tabs.onCreated.addListener(function(tab) {
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     tabAction();
 });
-
-
-/*
-var settings = new Store("settings", {
-    "sample_setting": "test"
-});
-*/

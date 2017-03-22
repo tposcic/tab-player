d008293f2f237e214b1428f2b2699f5d6e4e11fa
Toni <tposcic@riteh.hr>
2017-03-22 22:11:25 +0100

    oncreate onupdated eventi

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,22 +1,33 @@
 // if you checked "fancy-settings" in extensionizr.com, uncomment this lines
 
-var openTabs = new Array();
-chrome.tabs.query({}, function (tabs) {
-    for (var i = 0; i < tabs.length; i++) {
-        openTabs[i] = tabs[i];
-    }
-    // Moved code inside the callback handler
-    for (var i = 0; i < openTabs.length; i++) {
-        if (openTabs[i] != null)
-           window.console.log(openTabs[i].url);
-        else {
-            window.console.log("??" + i);
+function tabAction(){
+    var openTabs = new Array();
+
+    chrome.tabs.query({}, function (tabs) {
+        for (var i = 0; i < tabs.length; i++) {
+            openTabs[i] = tabs[i];
         }
-    }
+        // Moved code inside the callback handler
+        for (var i = 0; i < openTabs.length; i++) {
+            if (openTabs[i] != null && openTabs[i].url.includes("youtube")){
+            window.console.log(openTabs[i].url);
+            }
+        }
+    });
+
+}
+
+chrome.tabs.onCreated.addListener(function(tab) {         
+    tabAction();
 });
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    tabAction();
+});
+
+
 /*
 var settings = new Store("settings", {
     "sample_setting": "test"
 });
 */
-
